cbed1fb2cb4c367ed1503d707eff030bee0dffb3
mzulistiyan <izulistiyan@gmail,com>
2022-12-24 15:46:07 +0700

    update

--- lib/ui/pages/konsultasi/chat_page.dart
+++ lib/ui/pages/konsultasi/chat_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_myskin/bloc/dokter/bloc/update_status_konsultasi_bloc.dart';
 import 'package:flutter_application_myskin/ui/widget/CustomTooltipShape.dart';
 import 'package:flutter_application_myskin/model/response_konsultasi.dart';
+import 'package:flutter_application_myskin/ui/widget/custom_pop_loading.dart';
+import 'package:flutter_application_myskin/ui/widget/custom_rating.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:google_fonts/google_fonts.dart';
 
@@ -13,6 +17,8 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  final UpdateStatusKonsultasiBloc _updateStatusKonsultasiBloc =
+      UpdateStatusKonsultasiBloc();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,40 +36,78 @@ class _ChatPageState extends State<ChatPage> {
           ),
         ),
         actions: [
-          PopupMenuButton(
-            offset: const Offset(0, 50),
-            shape: const TooltipShape(),
-            onSelected: (result) {},
-            icon: const Icon(
-              Icons.more_horiz,
-              size: 30,
-              color: Colors.black,
-            ),
-            itemBuilder: (context) {
-              return [
-                PopupMenuItem(
-                  value: 0,
-                  child: Text(
-                    'Laporkan Dokter',
-                    style: GoogleFonts.poppins(
-                      fontSize: 12,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ),
-                PopupMenuItem(
-                  value: 1,
-                  child: Text(
-                    'Selesaikan Konsultasi',
-                    style: GoogleFonts.poppins(
-                      fontSize: 12,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ),
-              ];
-            },
-          )
+          widget.responseKonsultasi.statusKonsultasi == "SELESAI"
+              ? SizedBox()
+              : BlocConsumer<UpdateStatusKonsultasiBloc,
+                  UpdateStatusKonsultasiState>(
+                  bloc: _updateStatusKonsultasiBloc,
+                  listener: (context, state) {
+                    print('state is from UpdatePRofileBloc: $state');
+                    if (state is UpdateStatusKonsultasiLoading) {
+                      showDialog(
+                        context: context,
+                        barrierDismissible: false,
+                        builder: (context) {
+                          return const PopLoading();
+                        },
+                      );
+                    } else if (state is UpdateStatusKonsultasiSuccess) {
+                      showDialog(
+                        context: context,
+                        barrierDismissible: false,
+                        builder: (context) {
+                          return const RattingPopUp();
+                        },
+                      );
+                    }
+
+                    if (state is UpdateStatusKonsultasiError) {}
+                  },
+                  builder: (context, state) {
+                    return PopupMenuButton(
+                      offset: const Offset(0, 50),
+                      shape: const TooltipShape(),
+                      onSelected: (result) {
+                        if (result == 1) {
+                          _updateStatusKonsultasiBloc
+                              .add(UpdateStatusKonsultasiPost(
+                            statusKonsultasi: 'SELESAI',
+                            id: widget.responseKonsultasi.id!,
+                          ));
+                        }
+                      },
+                      icon: const Icon(
+                        Icons.more_horiz,
+                        size: 30,
+                        color: Colors.black,
+                      ),
+                      itemBuilder: (context) {
+                        return [
+                          PopupMenuItem(
+                            value: 0,
+                            child: Text(
+                              'Laporkan Dokter',
+                              style: GoogleFonts.poppins(
+                                fontSize: 12,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                          PopupMenuItem(
+                            value: 1,
+                            child: Text(
+                              'Selesaikan Konsultasi',
+                              style: GoogleFonts.poppins(
+                                fontSize: 12,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                        ];
+                      },
+                    );
+                  },
+                )
         ],
         leading: InkWell(
           onTap: () {
@@ -129,62 +173,78 @@ class _ChatPageState extends State<ChatPage> {
                   ),
                 ),
               ],
-            )
+            ),
+            const SizedBox(
+              height: 24,
+            ),
           ],
         )),
       ),
       bottomNavigationBar: Padding(
         padding: MediaQuery.of(context).viewInsets,
         child: Container(
-          padding: const EdgeInsets.all(12),
-          color: Colors.white,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const Icon(
-                Icons.add,
-                color: Colors.black54,
-              ),
-              const SizedBox(
-                width: 12,
-              ),
-              Expanded(
-                child: Container(
-                  height: 36,
-                  child: Center(
-                    child: TextField(
-                      decoration: InputDecoration(
-                        contentPadding: const EdgeInsets.all(8),
-                        hintText: 'Ketik Pesan Disini',
-                        hintStyle: GoogleFonts.poppins(fontSize: 12),
-                        fillColor: Colors.white,
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: const BorderSide(
-                              width: 1,
-                              color: Color(0xffE3E3E3)), //<-- SEE HERE
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: const BorderSide(
-                              width: 1,
-                              color: Color(0xffE3E3E3)), //<-- SEE HERE
+            padding: const EdgeInsets.all(12),
+            color: Colors.white,
+            child: widget.responseKonsultasi.statusKonsultasi == "SELESAI"
+                ? Text(
+                    'Sesi konsultasi dengan Dokter berakhir.',
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.poppins(
+                      fontSize: 10,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  )
+                : Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                        padding: EdgeInsets.zero,
+                        constraints: BoxConstraints(),
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.add,
+                          color: Colors.black54,
                         ),
                       ),
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                width: 12,
-              ),
-              const Icon(
-                Icons.send,
-                color: Colors.black54,
-              ),
-            ],
-          ),
-        ),
+                      const SizedBox(
+                        width: 12,
+                      ),
+                      Expanded(
+                        child: Container(
+                          height: 36,
+                          child: Center(
+                            child: TextField(
+                              decoration: InputDecoration(
+                                contentPadding: const EdgeInsets.all(8),
+                                hintText: 'Ketik Pesan Disini',
+                                hintStyle: GoogleFonts.poppins(fontSize: 12),
+                                fillColor: Colors.white,
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: const BorderSide(
+                                      width: 1,
+                                      color: Color(0xffE3E3E3)), //<-- SEE HERE
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: const BorderSide(
+                                      width: 1,
+                                      color: Color(0xffE3E3E3)), //<-- SEE HERE
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 12,
+                      ),
+                      const Icon(
+                        Icons.send,
+                        color: Colors.black54,
+                      ),
+                    ],
+                  )),
       ),
     );
   }
---
+++ lib/ui/widget/custom_rating.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class RattingPopUp extends StatelessWidget {
+  const RattingPopUp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      insetPadding: const EdgeInsets.symmetric(horizontal: 10),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(25),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(
+          top: 24,
+        ),
+        child: Container(
+          width: 100,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Rate your experience',
+                style: GoogleFonts.poppins(
+                  color: const Color(0xffEE7814),
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              Text(
+                'How would you rate\nDr. Mohamad Zulistiyan?',
+                textAlign: TextAlign.center,
+                style: GoogleFonts.poppins(),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              RatingBar.builder(
+                initialRating: 0,
+                minRating: 1,
+                direction: Axis.horizontal,
+                allowHalfRating: true,
+                itemCount: 5,
+                itemSize: 30,
+                itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+                itemBuilder: (context, _) => Icon(
+                  Icons.star,
+                  color: Colors.amber,
+                ),
+                onRatingUpdate: (rating) {
+                  print(rating);
+                },
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Container(
+                width: double.infinity,
+                height: 40,
+                decoration: BoxDecoration(
+                  color: const Color(0xffEE7814),
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(25),
+                    bottomRight: Radius.circular(25),
+                  ),
+                ),
+                child: Center(
+                  child: Text(
+                    'Submit',
+                    style: GoogleFonts.poppins(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
